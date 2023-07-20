723a8d67acad3eafec162dcd8f72e8ccf8ff2438
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-07-20 23:56:32 +0900

    update: add class file for management clusters

---
+++ VIRCE_server/src/RoomServer/ClusterManager.cs
@@ -0,0 +1,22 @@
+﻿namespace VIRCE_server.RoomServer;
+
+public class ClusterManager
+{
+    private static ClusterManager _instance;
+
+    public static ClusterManager Instance
+    {
+        get
+        {
+            return _instance ??= new ClusterManager();
+        }
+    }
+    
+    private ServerCluster _lobbyCluster;
+    private ServerCluster _miniGameCluster;
+    
+    private ClusterManager()
+    {
+        
+    }
+}
