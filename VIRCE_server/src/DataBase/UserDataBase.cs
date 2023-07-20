e397521927fd166c72d762e1c7f0df545c6937e6
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-07-20 23:57:21 +0900

    update: add class file for user database to make room function

---
+++ VIRCE_server/src/DataBase/UserDataBase.cs
@@ -0,0 +1,20 @@
+﻿using MasterMemory;
+namespace VIRCE_server.DataBase;
+
+public class UserDataBase
+{
+    private static UserDataBase _instance;
+
+    public static UserDataBase Instance
+    {
+        get
+        {
+            return _instance ??= new UserDataBase();
+        }
+    }
+    
+    private UserDataBase()
+    {
+        
+    }
+}
