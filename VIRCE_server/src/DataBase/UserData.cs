e2794f5aa15abe67c5dc093de269183f2e6cf3c7
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-07-21 20:57:26 +0900

    update: define database with Master Memory

---
+++ VIRCE_server/src/DataBase/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using MasterMemory;
+using MessagePack;
+
+namespace VIRCE_server.DataBase;
+
+[MemoryTable("UserData"), MessagePackObject(true)]
+public class UserData
+{
+    [PrimaryKey]
+    public int GlobalUserId { get; set; }
+    
+    [SecondaryKey(0), NonUnique]
+    public int UserId { get; set; }
+    
+    [SecondaryKey(1), NonUnique]
+    public int RoomId { get; set; }
+    
+    public string ModelId { get; set; }
+    
+    public string Name { get; set; }
+    
+    public IPEndPoint RemoteEndPoint { get; set; }
+}
