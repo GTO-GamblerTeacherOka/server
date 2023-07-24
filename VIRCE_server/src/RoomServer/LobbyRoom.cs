c8c5556d69ebb9e47852278525f58218e0e3c36f
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-07-24 19:18:54 +0900

    update: define override method

--- VIRCE_server/src/RoomServer/LobbyRoom.cs
+++ VIRCE_server/src/RoomServer/LobbyRoom.cs
@@ -1,4 +1,6 @@
-﻿using VIRCE_server.DataBase;
+﻿using System.Net;
+using VIRCE_server.DataBase;
+using VIRCE_server.PacketUtil;
 
 namespace VIRCE_server.RoomServer;
 
@@ -27,5 +29,11 @@ public class LobbyRoom : Server
         UdpClient?.Close();
         UdpClient = null;
         DataBaseManager.RemoveRoomServerInfo(ServerInfo);
+    }
+    
+    public override void OnReceive(IPEndPoint remoteEndPoint, byte[] data)
+    {
+        var parsedData = DataParser.Split(data);
+        var header = DataParser.AnalyzeHeader(parsedData.header);
     }
 }
