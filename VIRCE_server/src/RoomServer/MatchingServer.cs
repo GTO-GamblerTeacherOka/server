94e7d9593d58ea1dc2b39dc2a6ac25be1cc33d91
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-07-27 09:25:41 +0900

    update: add constructor logic and add method Stop and ReceiveStart, and add variable to Run MatchingServer

--- VIRCE_server/src/RoomServer/MatchingServer.cs
+++ VIRCE_server/src/RoomServer/MatchingServer.cs
@@ -1,8 +1,19 @@
-﻿namespace VIRCE_server.RoomServer;
+﻿using System.Net.Sockets;
+using Cysharp.Threading.Tasks;
+using VIRCE_server.PacketUtil;
+
+namespace VIRCE_server.RoomServer;
 
 public class MatchingServer
 {
-    private static MatchingServer _instance;
+    private static MatchingServer? _instance;
+    
+    private readonly ServerCluster _lobbyServerCluster;
+    private readonly ServerCluster _miniGameServerCluster;
+
+    private const int Port = 5000;
+    private readonly UdpClient _udpClient;
+    public bool IsRunning { get; private set; }
 
     public static MatchingServer Instance
     {
@@ -14,8 +25,44 @@ public class MatchingServer
     
     private MatchingServer()
     {
-        
+        _lobbyServerCluster = new LobbyRoomCluster();
+        _miniGameServerCluster = new MiniGameRoomCluster();
+        _udpClient = new UdpClient(Port);
+        IsRunning = false;
     }
-    
-    
+
+    private void Start()
+    {
+        _lobbyServerCluster.Start();
+        _miniGameServerCluster.Start();
+        IsRunning = true;
+        ReceiveStart().Forget();
+    }
+
+    private void Stop()
+    {
+        IsRunning = false;
+    }
+
+    private async UniTask ReceiveStart()
+    {
+        while (true)
+        {
+            if (IsRunning is false) break;
+            var data = await _udpClient.ReceiveAsync();
+            var (rawHeader,rawBody) = DataParser.Split(data.Buffer);
+            var header = DataParser.AnalyzeHeader(rawHeader);
+            if (header.flag != DataParser.Flag.RoomEntry) continue;
+            var roomType = BitConverter.ToInt32(rawBody);
+            switch (roomType)
+            {
+                case 0:
+                    _lobbyServerCluster.Entry(data.RemoteEndPoint);
+                    break;
+                case 1:
+                    _miniGameServerCluster.Entry(data.RemoteEndPoint);
+                    break;
+            }
+        }
+    }
 }
