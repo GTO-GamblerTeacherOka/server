25f2884d2b978cae235bc5f67578fcf18f926906
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-09-20 21:27:23 +0900

    update: add file and environment variables for initialize mysql and fix bug can't connect redis

---
+++ MySQL/Init.sql
@@ -0,0 +1,29 @@
+﻿CREATE DATABASE IF NOT EXISTS `virce`;
+USE `virce`;
+
+CREATE TABLE IF NOT EXISTS `RoomType` (
+    `TypeID` TINYINT NOT NULL,
+    `TypeDetail` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`TypeID`)
+);
+
+CREATE TABLE IF NOT EXISTS `Room` (
+    `RoomID` INT NOT NULL AUTO_INCREMENT,
+    `RoomType` TINYINT NOT NULL,
+    FOREIGN KEY (`RoomType`) REFERENCES `RoomType`(`TypeID`),
+    PRIMARY KEY (`RoomID`)
+);
+
+CREATE TABLE IF NOT EXISTS `User` (
+    `UserID` TINYINT NOT NULL,
+    `RoomID` INT NOT NULL,
+    FOREIGN KEY (`RoomID`) REFERENCES `Room`(`RoomID`),
+    `ModelID` TEXT NOT NULL,
+    `DisplayName` TEXT NOT NULL,
+    `IPAddress` TEXT NOT NULL,
+    `Port` INT NOT NULL,
+    PRIMARY KEY (`UserID`, `RoomID`)
+);
+
+INSERT IGNORE INTO `RoomType` (`TypeID`, `TypeDetail`) VALUES (0, 'Lobby');
+INSERT IGNORE INTO `RoomType` (`TypeID`, `TypeDetail`) VALUES (1, 'Lace');
