ebf8d8f1d699203899333f3abef7c7ec0b6658bf
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-09-21 20:46:22 +0900

    add: socket class

---
+++ VIRCE_server/src/Server/Socket.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Cysharp.Threading.Tasks;
+
+namespace VIRCE_server.Server;
+
+public static class Socket
+{
+    private static readonly UdpClient UdpClient;
+
+    static Socket()
+    {
+        UdpClient = new UdpClient(5000);
+    }
+
+    public static async UniTask SendAsync(byte[] data, IPEndPoint endPoint)
+    {
+        await UdpClient.SendAsync(data, data.Length, endPoint);
+    }
+
+    public static async UniTask<UdpReceiveResult> ReceiveAsync()
+    {
+        return await UdpClient.ReceiveAsync();
+    }
+}
