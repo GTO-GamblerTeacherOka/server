7aa636077e079c572e885b7db1195b8bf3b2ff61
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-09-27 00:04:47 +0900

    update: change ttl 100 to 1 and change op "?" to "!" when use nullable variable

--- VIRCE_server/src/Controller/RedisController.cs
+++ VIRCE_server/src/Controller/RedisController.cs
@@ -12,60 +12,61 @@ public static class RedisController
     {
         _redis = ConnectionMultiplexer.Connect(Env.GetString("REDIS_HOST"));
         _db = _redis.GetDatabase();
+        Remove("matching");
     }
 
     public static void SetString(in string key, in string value)
     {
-        _db?.StringSet(key, value);
+        _db!.StringSet(key, value);
     }
 
     public static string GetString(in string key)
     {
-        return (_db?.StringGet(key) ?? string.Empty)!;
+        return _db!.StringGet(key)!;
     }
 
     public static void SetHash(in string key, in HashEntry[] hashEntries)
     {
-        _db?.HashSet(key, hashEntries);
+        _db!.HashSet(key, hashEntries);
     }
 
     public static HashEntry[] GetHash(in string key)
     {
-        return _db?.HashGetAll(key) ?? Array.Empty<HashEntry>();
+        return _db!.HashGetAll(key);
     }
 
     public static void SetList(in string key, in string[] values)
     {
-        _db?.ListRightPush(key, values.Select(RedisValue.Unbox).ToArray());
+        _db!.ListRightPush(key, values.Select(RedisValue.Unbox).ToArray());
     }
 
     public static void SetList(in string key, in string value)
     {
-        _db?.ListRightPush(key, value);
+        _db!.ListRightPush(key, value);
     }
 
     public static string[] GetList(in string key)
     {
-        return (_db?.ListRange(key) ?? Array.Empty<RedisValue>()).Select(x => x.ToString()).ToArray();
+        return _db!.ListRange(key).Select(x => x.ToString()).ToArray();
     }
 
     public static void Remove(in string key)
     {
-        _db?.KeyDelete(key);
+        _db!.KeyDelete(key);
     }
 
     public static void Remove(in string[] keys)
     {
-        _db?.KeyDelete(keys.Select(x => (RedisKey)x).ToArray());
+        _db!.KeyDelete(keys.Select(x => (RedisKey)x).ToArray());
     }
 
     public static void Remove(in string key, in string value)
     {
-        _db?.ListRemove(key, value);
+        _db!.ListRemove(key, value);
     }
 
     public static bool SetNx(in string key)
     {
-        return _db?.StringSet(key, "1", TimeSpan.FromSeconds(100), When.NotExists, CommandFlags.None) ?? false;
+        return _db!.StringSet(key, "1", TimeSpan.FromSeconds(1), When.NotExists, CommandFlags.None);
     }
 }
