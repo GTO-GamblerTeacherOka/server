0c4acbf8a4d44e1c3bacf061fddf87ca34605a5b
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-10-12 12:31:12 +0900

    hotfix: Tcp Listener for healthcheck

--- VIRCE_server/src/HealthCheck/HealthCheckReceiver.cs
+++ VIRCE_server/src/HealthCheck/HealthCheckReceiver.cs
@@ -1,17 +1,19 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace VIRCE_server.HealthCheck;
 
 public static class HealthCheckReceiver
 {
-    private static readonly TcpClient Client = new();
+    private static readonly TcpListener Client;
 
     static HealthCheckReceiver()
     {
-        Client.Connect("localhost", 8080);
+        Client = new TcpListener(IPAddress.Any, 8000);
     }
 
     public static void Initialize()
     {
+        Client.Start();
     }
 }
